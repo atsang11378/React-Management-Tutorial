e15c8214c09125c0439cb9ddecd4835a4c064111
atsang11378 <atsang11378@daum.net>
2020-05-01 16:19:55 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-backgrond">
-      <img src={logo} lat = "logo" />
-      <h2>Let's develp management system !</h2>
-    </div>
-  );
+const customers =[ {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '춘향이',
+  'birthday' : '961222',
+  'gender' : '여자',
+  'job' : '대학생'
+},
+
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍길동',
+  'birthday' : '951222',
+  'gender' : '남자',
+  'job' : '프로그래머'
+},
+
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '구름이',
+  'birthday' : '941221',
+  'gender' : '남자',
+  'job' : '디자이너'
 }
+]
 
+class App extends Component {
+  render(){
+     return ( 
+       <div>
+         {
+          customers.map( c => {
+            return (
+                <Customer
+                  key = {c.id}
+                  image = {c.image}
+                  name = {c.name}
+                  birthday = {c.birthday}
+                  gender = {c.gender}
+                  job = {c.job}
+                  />
+
+            );
+          })
+          }
+          </div>
+
+    );
+  }
+}
 export default App;
   
