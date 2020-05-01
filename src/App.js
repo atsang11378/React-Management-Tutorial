34d976d8485036d0d31e580ecb18f5e0a4f17998
atsang11378 <atsang11378@daum.net>
2020-05-01 14:50:19 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-backgrond">
+      <img src={logo} lat = "logo" />
+      <h2>Let's develp management system !</h2>
+    </div>
+  );
+}
+
+export default App;
+  
